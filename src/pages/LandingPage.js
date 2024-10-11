f0388e4c6cc45071ed31ad4967b93a31d56e85a1
Badri Narayana Tulsi Ram Iyengar <krsna.iyengar@gmail.com>
2024-10-11 15:54:46 +0530

    removed fly.io files

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,10 +3,12 @@ import PeacockFeather from '../assets/PeacockFeather.jpeg'
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
-      <h1>Welcome to krsna.love</h1>
-      <img className="main-photo" src={PeacockFeather} />
-      <p>Discover my portfolio of projects and more.</p>
+    <div className="landing-container">
+        <div className="landing-page">
+            <h1>Welcome to krsna.love</h1>
+            <img className="main-photo" src={PeacockFeather} />
+            <p>Discover my portfolio of projects and more.</p>
+        </div>
     </div>
   );
 };
--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 const PortfolioPage = () => {
-  return (
-    <div className="portfolio-page">
-      <h1>My Portfolio</h1>
-      <p>Here is a collection of my work.</p>
-      <iframe class="iFrame" src="https://badri2.tumblr.com"></iframe>
-    </div>
-  );
+
+    const projects = [
+        { id: 1, title: "Tumblr Blog", description: "I created this blog as a hobby while studying Religious Studies at the University of California, Santa Barbara", url: "badri2.tumblr.com" },
+        { id: 2, title: "AI Trader", description: "A full-stack web app I am working on using the PERN Stack", url: "ai-trader.in" },
+        // Add more projects here
+      ];
+
+    return (
+        <div className="portfolio-page">
+            <h1>My Portfolio</h1>
+            <p>Here is a collection of my work.</p>
+            <div className="project-grid">
+                <div className="project-card">
+                    <iframe class="iFrame" src="http://badri2.tumblr.com"/>
+                    <h3>Tumblr Blog</h3>
+                    <p>I created this blog as a hobby while studying Religious Studies at the University of California, Santa Barbara</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PortfolioPage;
